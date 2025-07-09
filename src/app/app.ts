f342ec39df8a8bb88394840a4c8e3f10d1ef3611
Taynara Ribeiro <taynara.rsouza@gmail.comm>
2025-07-08 21:37:20 -0300

    Versão do dia 08/07/25

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { Header } from './componentes/header/header';
+import { Footer } from './componentes/footer/footer';
+import { Filme } from './componentes/filme/filme';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [ Header, Footer, Filme],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
